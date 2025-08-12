6245673abeb905793c6e881994d5a5c37d97777a
lm105079 <matthieu.lentini@etu.unice.fr>
2025-08-12 21:33:21 +0200

    déploiement 1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
+
+  // Servir les fichiers Angular
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  // Pour toutes les routes non-API, renvoyer index.html
+  app.getHttpAdapter().get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
