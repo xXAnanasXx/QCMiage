8e6aa6394fefaf151e2862bcc6f930518e91315d
ClementGorbatch <116630987+ClementGorbatch@users.noreply.github.com>
2025-08-24 22:50:52 +0200

    Page connection 1er commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,16 +9,16 @@ import {TypeOrmModule} from '@nestjs/typeorm';
         ConfigModule.forRoot({
             isGlobal: true, // pour que ce soit disponible partout
         }),
-        // TypeOrmModule.forRoot({
-        //     type: 'postgres',
-        //     host: process.env.DB_HOST,
-        //     port: parseInt(process.env.DB_PORT),
-        //     username: process.env.DB_USER,
-        //     password: process.env.DB_PASSWORD,
-        //     database: process.env.DB_NAME,
-        //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        //     synchronize: true, // à désactiver en prod
-        // }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true, // à désactiver en prod
+        }),
     ],
     controllers: [AppController],
     providers: [AppService],
